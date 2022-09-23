c747d2888898a4e408d988b54df4e8f8ff5f081d
Tao-Zonyo <zytao28@icloud.com>
2022-09-23 16:28:50 +0800

    chore: add a few comments - tweak tsconfig.json and .eslintrc - resolve errors after the tweak

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('markdown-outline-helper.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('markdown-outline-helper.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from markdown-outline-helper!');
@@ -23,4 +23,5 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 export function deactivate() {}
